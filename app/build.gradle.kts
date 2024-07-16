41ce2233c81ece6ccdff7a312072c021d9a82cd7
Iliatar <a.u.tarasov@gmail.com>
2024-07-16 07:54:25 +0300

    add checkstyle and jacoco

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,10 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
 plugins {
-    id("java")
+    java
+    checkstyle
+    jacoco
 }
 
 group = "hexlet.code"
@@ -16,4 +21,14 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+    // https://technology.lastminute.com/junit5-kotlin-and-gradle-dsl/
+    testLogging {
+        exceptionFormat = TestExceptionFormat.FULL
+        events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+        // showStackTraces = true
+        // showCauses = true
+        showStandardStreams = true
+    }
 }
+
+tasks.jacocoTestReport { reports { xml.required.set(true) } }
