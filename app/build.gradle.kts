0ce95dd2002e97efb36d924d7a46b47a2a2d1603
Iliatar <a.u.tarasov@gmail.com>
2024-07-17 08:00:53 +0300

    Hello world works!

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,9 +2,16 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
+    application
     java
     checkstyle
     jacoco
+    id("io.freefair.lombok") version "8.6"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+}
+
+application {
+    mainClass.set("hexlet.code.App")
 }
 
 group = "hexlet.code"
@@ -17,6 +24,11 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("io.javalin:javalin:6.1.3")
+    implementation("gg.jte:jte:3.1.9")
+    implementation("io.javalin:javalin-rendering:6.1.3")
+    implementation("io.javalin:javalin-bundle:6.1.3")
+    implementation("org.slf4j:slf4j-simple:2.0.7")
 }
 
 tasks.test {
